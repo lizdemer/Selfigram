8bc4f6551c4bd37cf5c658a8e791d20be8e90df2
lizdemer <300156501@Administrators-Mac-mini.local>
2016-12-01 20:11:43 -0800

    Dec 1 2016

--- Selfigram/Post.swift
+++ Selfigram/Post.swift
@@ -10,12 +10,22 @@ import Foundation
 import UIKit
 
 class Post {
-    let image:UIImage
+    //LIZ: The optional ?'s below can be removed next week, along with let image:UIImage
+    let imageURL:URL?
     let user:User
     let comment:String
-    init(image:UIImage, user:User, comment:String){
-        self.image = image
+    let image:UIImage?
+    
+    init(imageURL:URL?, user:User, comment:String, image:UIImage?){
+        // You can name the property you are passing into the function the
+        // same name as the class' property. To distinguish the two
+        // add "self." to the beginning of the class' property.
+        // So for example we are passing in an UImage called image and setting it as our
+        // image property for Post.
+        self.imageURL = imageURL
         self.user = user
         self.comment = comment
+        self.image = image
     }
+    
 }
